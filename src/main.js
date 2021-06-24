e4b149302951c3fd414efbf29600bef14509bc1d
Samuel Cifuentes García <uo238524@uniovi.es>
2021-06-24 19:57:13 +0200

    Passwords displayed in a grid with the matching sprites and CSS taking form

--- src/main.js
+++ src/main.js
@@ -6,27 +6,57 @@ function onClickEvent(input) {
     $("#output_area").empty();
 
     passwords = pw.generatePasswords(input);
-    console.log(passwords)
+    console.log(passwords);
 
-    difficulties = ["First Quest", "Second Quest"]
+    difficulties = ["First Quest", "Second Quest"];
+    stages = ["Stage 1", "Stage 2", "Stage 3", "Stage 4", "Stage 5",
+            "Stage 6", "Stage 7", "Stage 8", "Stage 9", "Stage A",
+            "Stage B", "Slogra", "Gaibon", "Death", "Dracula"]
     for (let i = 0; i < difficulties.length; i++) {
-        $("#output_area").append("<h2>"+difficulties[i]+"</h2>");
+        $("#output_area").append("<h2>" + difficulties[i] + "</h2>");
+        $("#output_area").append("<div class='quest_area' id='quest_" + i+1 +"'></div>")
         for (let j = 0; j < passwords[i].length; j++) {
-            formatted_p = textMatrix(passwords[i][j])
-            $("#output_area").append("<pre>Level " + j + "\n" + formatted_p + "</pre>");
+            password_table = passwordTable(passwords[i][j]);
+            password_container = "<div class='password_container'><h3>" + stages[j] +"</h3>"
+            $("#quest_" + i+1).append(password_container + password_table + "</div>");
         }
 
     }
 }
 
-    function textMatrix(password) {
-        var formatted_string = ""
-        for (let i = 0; i < 4; i++) {
-            for (let j = 0; j < 4; j++) {
-                formatted_string += password[i * 4 + j] + " ";
-            }
-            formatted_string += "\n"
+function textMatrix(password) {
+    var formatted_string = "";
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            formatted_string += password[i * 4 + j] + " ";
         }
-
-        return formatted_string
+        formatted_string += "\n";
     }
+
+    return formatted_string
+}
+
+function passwordTable(password){
+    var table = "<table class='password'>";
+    for (let i = 0; i <4; i++){
+        table += "<tr>";
+        for (let j = 0; j < 4; j++){
+            table += "<td>"
+            switch (password[i * 4 + j]){
+                case "A":
+                    table += "<img src='img/axe.png' alt='Axe'/>";
+                    break;
+                case "W":
+                    table += "<img src='img/holy_water.png' alt='Holy Water'/>";
+                    break;
+                case "H":
+                    table += "<img src='img/heart.png' alt='Heart'/>";
+                    break;
+            }
+            table +="</td>"
+        }
+        table +="</tr>";        
+    }
+    table += "</table>";
+    return table;
+}
